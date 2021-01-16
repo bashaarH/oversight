f938354b8d5ea0b777d40fd5deed747773252403
BashManVP <47926113+BashManVP@users.noreply.github.com>
2021-01-16 17:29:52 -0500

    Update views.py

--- manager/views.py
+++ manager/views.py
@@ -154,10 +154,10 @@ def get_google_calendar_events():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                r'manager\credentials.json', SCOPES)
+                'manager/credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open(r'manager\token.pickle', 'wb') as token:
+        with open('manager/token.pickle', 'wb') as token:
             pickle.dump(creds, token)
 
     service = build('calendar', 'v3', credentials=creds)
