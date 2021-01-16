d2153d87f107d0fec51ac72be2e24870bf2a37e8
BashManVP <47926113+BashManVP@users.noreply.github.com>
2021-01-16 18:42:47 -0500

    summary

--- manager/views.py
+++ manager/views.py
@@ -16,6 +16,7 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 import datetime
 import pycronofy
+import random
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
@@ -145,9 +146,9 @@ def get_google_calendar_events():
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
+    
+    with open('manager/token.pickle', 'rb') as token:
+        creds = pickle.load(token)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
@@ -210,8 +211,10 @@ def get_outlook_calendar_events():
 
     for event in events.all():
         if event['summary'] not in holidays:
-            upcoming.append((event['summary'], datetime.datetime.strptime(event['start'][:19], '%Y-%m-%dT%H:%M:%S'), datetime.datetime.strptime(event['end'][:19], '%Y-%m-%dT%H:%M:%S')))
-
-    return (upcoming)
+            try:
+                upcoming.append((event['summary'], datetime.datetime.strptime(event['start'][:19], '%Y-%m-%dT%H:%M:%S'), datetime.datetime.strptime(event['end'][:19], '%Y-%m-%dT%H:%M:%S'), random.choice(["blue", "red", "green"]), True))
+            except ValueError:
+                upcoming.append((event['summary'], datetime.datetime.strptime(event['start'][:19], '%Y-%m-%d'), datetime.datetime.strptime(event['end'][:19], '%Y-%m-%d'), random.choice(["blue", "red", "green"]), False))
+    return(upcoming)
 
 
